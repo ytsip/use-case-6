2e595aa870aee31f6cdf3dde506c8bbb30f0d1ec
Yan Tsipun <yan.tsipun@youscience.com>
2023-08-17 19:34:27 +0300

    UC#6 Application Creation

--- upload-lambda-code.sh
+++ upload-lambda-code.sh
@@ -3,3 +3,6 @@ func_name=$1
 aws lambda update-function-code \
     --function-name "${func_name}" \
     --zip-file fileb://handler.zip
+aws lambda update-function-configuration \
+    --function-name "${func_name}" \
+    --handler handler.lambda_handler
